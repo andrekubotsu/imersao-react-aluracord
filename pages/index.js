ac0907077a785a95861e8be4f0f4153d12ef3a85
Andre Kubotsu <contato201@gmail.com>
2022-01-24 08:40:50 -0300

    css in js examples from next

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return (
+        <div>
+            <h1>Boas vindas de volta!</h1>
+            <h2>Discord - Alura Matrix</h2>
+            {/* CSS in JS from Next itself */}
+            <style jsx>{`
+                h1 {
+                    color: red;
+                }
+            `}</style>
+        </div>
+    )
 }
 
 export default HomePage
